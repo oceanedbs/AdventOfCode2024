7748ea1eb6124eca98c9c7c60f461c37b78186eb
Océane Dubois <dubois@isir.upmc.fr>
2024-12-09 16:57:08 +0100

    Day 9 part 2 in progress

--- Day9/day9.py
+++ Day9/day9.py
@@ -1,4 +1,6 @@
-with open('Day9/data.txt', 'r') as file:
+from operator import itemgetter
+
+with open('Day9/data_test.txt', 'r') as file:
     data = file.read()
 
 print(data)
@@ -6,37 +8,86 @@ print(data)
 # Part 1
 
 i = 0
-res=''
+res = []
 
 for k, char in enumerate(data):
-    if k%2 == 0:
-        res+= str(i)*int(char)
-        i+=1
+    if k % 2 == 0:
+        res.extend([i] * int(char))
+        i += 1
     else:
-        res += '.'*int(char)
-print(res[0:200])
-print(res[-200:-1])
+        res.extend(['.'] * int(char))
 
-res_list = list(res)
+print(res[0:100])
 
-last_digit_index = len(res_list) - 1
-for i in range(0, len(res_list)):
-    if res_list[i] == '.':
+
+
+last_digit_index = len(res) - 1
+for i in range(0, len(res)):
+    if res[i] == '.':
         not_placed = True
         while last_digit_index > i and not_placed:
-            if res_list[last_digit_index].isdigit():
-                res_list[i] = res_list[last_digit_index]
-                res_list[last_digit_index] = '.'
+            if isinstance(res[last_digit_index], int):
+                res[i] = res[last_digit_index]
+                res[last_digit_index] = '.'
                 not_placed = False
             last_digit_index -= 1
                 
-
-result_string = ''.join(res_list)
+# Convert all integers in res to strings
+res = [str(x) for x in res]
+result_string = ''.join(res)
 print(result_string[0:200])
 
 total=0
-for k, char in enumerate(res_list):
-    if(res_list[k]!='.'):
-        total+= int(res_list[k])*k
+for k, char in enumerate(res):
+    if(res[k]!='.'):
+        total+= int(res[k])*k
 
 print(total)
+
+
+#Part 2
+
+
+def reconstruct_list(data_blocks) :
+    #reconstruct the list based on the data_blocks
+    res = ['.'] * len(data)
+    for db in data_blocks:
+        res[db[2]:db[2] + db[1]] = [db[0]] * db[1]
+    return res
+
+print('Part 2') 
+i = 0
+res = []
+data_blocks = []
+data_blanks = []
+for k, char in enumerate(data):
+    size = len(res)
+    if k % 2 == 0:
+        res.extend([i] * int(char))
+        data_blocks.append([i, int(char), size])        
+        i += 1
+
+    else:
+        res.extend(['.'] * int(char))
+        data_blanks.append([int(char), size])
+print(res)
+
+print('blanks : ', data_blanks)
+print('blocks : ', data_blocks)
+
+for db in reversed(data_blocks):
+    for b in data_blanks:
+        if db[1] <= b[0]:
+            print('Change block', db, 'to blank', b)
+            db[2] = b[1]
+            b[0] = b[0] - db[1]
+            data_blocks = sorted(data_blocks, key=itemgetter(2))
+            print('Sorted blocks : ', data_blocks)
+            print(reconstruct_list(data_blocks))
+            break
+            
+
+print('blanks : ', data_blanks)
+print('blocks : ', data_blocks)
+
+
