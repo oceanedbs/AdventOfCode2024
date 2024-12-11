83df4acb902b9c91264aef9b02cd74b60b8ef6e3
oceanedbs <oceane.tribudubois@gmail.com>
2024-12-11 10:08:36 +0100

    Day 11 part 1 non optimized

--- Day11/day11.py
+++ Day11/day11.py
@@ -5,24 +5,26 @@ def read_data(file_path):
         data = file.read().strip().split()
         return [int(num) for num in data]
 
-data = read_data('Day11/data_test.txt')
+data = read_data('Day11/data.txt')
 print(data)
 
 # Part 1
-for i in np.arange(6):
-    size = len(data)
-    for n in np.arange(size):
-        print(data[n])
-        if data[n] == 0:
-            data[n] = 0
-        elif len(str(data[n]))% 2 == 0:
-            mid = len(str(data[n])) // 2
-            first_half = int(str(data[n])[:mid])
-            second_half = int(str(data[n])[mid:])
-            data[n] = first_half
-            data.insert(n + 1, second_half)
+for i in np.arange(25):
+    added = False
+    for p in data:
+        if added == True:
+            added = False
+            continue
+        if p == 0:
+            data[data.index(p)] = 1
+        elif len(str(p))% 2 == 0:
+            mid = len(str(p)) // 2
+            first_half = int(str(p)[:mid])
+            second_half = int(str(p)[mid:])
+            data[data.index(p)] = first_half
+            data.insert(data.index(first_half) + 1, second_half)
+            added = True
         else:
-            data[n] = data[n]*2024
-    print(data)
+            data[data.index(p)]= p*2024
 
 print(len(data))
